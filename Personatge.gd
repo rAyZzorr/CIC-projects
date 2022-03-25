eda938530923f1d6cb5379d4eef99e7fc7c20011
rAyZzorr <85404411+rAyZzorr@users.noreply.github.com>
2022-03-25 19:40:47 +0100

    moviment i vainas varias escena 1 casi done

--- Personatge.gd
+++ Personatge.gd
@@ -1,11 +1,14 @@
 extends KinematicBody2D
-var dins = false
+
+var llave = false
 var velocitat_base = 200
 var velocitat = Vector2.ZERO
-var gravetat = Vector2.DOWN * 10
-var salt = Vector2.UP * 20
-var segons_dins = 0
+var gravetat = Vector2.DOWN * 980
+var salt = Vector2.UP * 400
 
+
+func _ready():
+	position = Vector2(40, 560)
 func _physics_process(delta):
 	velocitat.x = 0
 	if Input.is_action_pressed("mou_dreta"):
@@ -14,6 +17,7 @@ func _physics_process(delta):
 		velocitat += Vector2.LEFT * velocitat_base
 	if Input.is_action_just_pressed("mou_amunt") and is_on_floor():
 		velocitat += salt
+	velocitat += gravetat * delta
 	velocitat = move_and_slide(velocitat, Vector2.UP)
 	anima(velocitat)
 	
@@ -26,39 +30,35 @@ func anima(velocitat: Vector2):
 		animacio.flip_h = true
 		animacio.play('camina')
 
-#	if velocitat.y < -1:
-#		animacio.play("salta")
-
+	if velocitat.y < -1:
+		animacio.play("salta")
 	if abs(velocitat.x) < 0.5:
 		animacio.play('default')
 
+func _on_Lava_body_entered(body):
+	position = Vector2(40, 560)
+#	get_tree().quit()
 
-func _on_Portal_abaix_body_entered(body):
-	position = Vector2(240,140)
+func _on_Escaleras_body_entered(body):
+	position = Vector2(950, 225)
+	salt = Vector2.UP * 600
 
-func _on_Portal_Dalt_body_entered(body):
-	position = Vector2(860,300)
+func _on_problemasp2_body_entered(body):
+	position = Vector2(40, 560)
+	llave = false
+	salt = Vector2.UP * 450
+	if llave == true:
+		get_node("llave").show()
+		
+func _on_llave_body_exited(body):
+	get_node("llave/clau").hide()
+	llave = true
+	
 
-
-
-
-func _on_Area2D_body_entered(body):
-	dins = true
-	velocitat_base = 100
-	segons_dins = 0
-	modulate = Color(1,0.5,0)
-	$Timer.start()
-func _on_Timer_timeout():
-	segons_dins += 1
-
-	if segons_dins % 2 != 0:
-		modulate = Color(1,1,1)
-		velocitat_base = 200
+func _on_puerta_body_entered(body):
+	if llave == true:
+		get_tree().change_scene("res://sc2.tscn")
 	else:
-		modulate = Color(1,0.5,0)
-func _on_Area2D_body_exited(body):
-	segons_dins = 0
-	modulate = Color(1,1,1)
-	dins = false
-	$Timer.stop()
+		position = Vector2(950, 225)
+
 
