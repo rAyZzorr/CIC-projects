096b1e12c6a1da69b58e66b050770ca36c9e0b0a
rAyZzorr <85404411+rAyZzorr@users.noreply.github.com>
2022-03-27 01:38:30 +0100

    NOUS CANVIS
    asteroide 11,18, 15 ,6 codi fora error raro pregunta jordi
    
    preguntar si podem cambiar el move and slide per un sense slide
    
    perque puc reventar asteroides amb el cap?
    
    portal a sc3 no rula
    
    asteroides que no es mouen wtf??

--- Personatge.gd
+++ Personatge.gd
@@ -8,7 +8,7 @@ var salt = Vector2.UP * 400
 
 
 func _ready():
-	position = Vector2(40, 560)
+	position = Vector2(40, 520)
 func _physics_process(delta):
 	velocitat.x = 0
 	if Input.is_action_pressed("mou_dreta"):
@@ -20,7 +20,17 @@ func _physics_process(delta):
 	velocitat += gravetat * delta
 	velocitat = move_and_slide(velocitat, Vector2.UP)
 	anima(velocitat)
-	
+	for index in get_slide_count():
+		var collision = get_slide_collision(index)
+		if collision.collider.name.begins_with("asteroid"):
+			var ruta = "/root/sc2/asteroides/%s"
+			var node = ruta % collision.collider.name
+
+			if get_node(node).get("estado") == true:
+#			if get_node("/root/sc2/asteroides/asteroid1").get("estado") == true:
+				position = Vector2(40, 520)
+			else:
+				pass
 func anima(velocitat: Vector2):
 	var animacio = $AnimatedSprite
 	if velocitat.x > 0:
@@ -35,30 +45,28 @@ func anima(velocitat: Vector2):
 	if abs(velocitat.x) < 0.5:
 		animacio.play('default')
 
-func _on_Lava_body_entered(body):
-	position = Vector2(40, 560)
+func _on_Lava_body_entered(_body):
+	position = Vector2(40, 520)
 #	get_tree().quit()
 
-func _on_Escaleras_body_entered(body):
+func _on_Escaleras_body_entered(_body):
 	position = Vector2(950, 225)
 	salt = Vector2.UP * 600
 
-func _on_problemasp2_body_entered(body):
-	position = Vector2(40, 560)
+func _on_problemasp2_body_entered(_body):
+	position = Vector2(40, 520)
 	llave = false
 	salt = Vector2.UP * 450
-	if llave == true:
-		get_node("llave").show()
-		
-func _on_llave_body_exited(body):
-	get_node("llave/clau").hide()
-	llave = true
 	
-
-func _on_puerta_body_entered(body):
+		
+func _on_llave_body_exited(_body):
+	llave = true
+	print(llave)
+func _on_puerta_body_entered(_body):
 	if llave == true:
 		get_tree().change_scene("res://sc2.tscn")
 	else:
 		position = Vector2(950, 225)
 
-
+#func _on_portal_1_body_entered(_body):
+#	get_tree().change_scene("res://sc3.tscn")
---
+++ asteroid17.gd
@@ -0,0 +1,43 @@
+extends KinematicBody2D
+
+
+var velocitat_base = 200
+var velocitat = Vector2.ZERO
+var gravetat = Vector2.DOWN * 50
+var estado = false
+
+
+func _ready():
+	anima("ready")
+	position = Vector2(370, 100)
+func _physics_process(delta):
+	if is_on_floor():
+		for index in get_slide_count():
+			var collision = get_slide_collision(index)
+			if collision.collider.name.begins_with("personatge"):
+				anima("destroy")
+				position = Vector2(370, 560)
+				get_node("CollisionPolygon2D").set_deferred("disabled", true)
+				estado = true
+			elif collision.collider.name.begins_with("asteroid"):
+				velocitat += gravetat * delta
+				velocitat = move_and_slide(velocitat, Vector2.UP)
+				anima("ready")
+			else:
+				anima("destroy")
+				estado = false
+				get_node("CollisionPolygon2D").set_deferred("disabled", true)
+	else:
+		velocitat += gravetat * delta
+		velocitat = move_and_slide(velocitat, Vector2.UP)
+		anima("ready")
+		
+		
+func anima(options):
+	var animacio = $AnimatedSprite
+	if options == "ready":
+		animacio.play('default')
+	elif options == "destroy":
+		animacio.play('destroy')
+		if animacio.frame == 3:
+			animacio.stop()
