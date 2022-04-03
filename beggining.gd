80d6a6fba46db276ad4f568fbdeaf3b1045e4302
rAyZzorr <85404411+rAyZzorr@users.noreply.github.com>
2022-04-03 21:09:30 +0200

    blablabla finished

--- beggining.gd
+++ beggining.gd
@@ -5,3 +5,7 @@ extends Node2D
 
 func _on_TextureButton_pressed():
 	get_tree().change_scene("res://sc1.tscn")
+
+
+func _on_TextureButton2_pressed():
+	get_tree().change_scene("res://htp.tscn")
---
+++ htp.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+
+
+
+
+func _on_back_pressed():
+# warning-ignore:return_value_discarded
+	get_tree().change_scene("res://beggining.tscn")
